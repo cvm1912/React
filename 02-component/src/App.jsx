e5eded8934602235369237edbf4b203bc564958d
cvm1912 <shivam.fcse@gmail.com>
2025-11-17 20:14:12 +0530

    feat(component): added git add .

---
+++ 02-component/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from "./Components/card";
+import Navbar from "./Components/navbar";
+
+const App = () => {
+
+  const course = "React JS Development"
+
+  return (
+    <>
+    <Navbar/>
+    <Card/>
+    </>
+  );
+};
+
+export default App;
---
+++ 02-component/src/Components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+     <div className='card'>
+        <div className="content">
+            <h1>Shivam singh</h1>
+            <p>Full Stack Developer</p>
+        </div>
+      </div>
+  )
+}
+
+export default card
