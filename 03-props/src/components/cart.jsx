210fff6974514d9da7f55631210357fd36db4380
cvm1912 <shivam.fcse@gmail.com>
2025-11-19 16:42:28 +0530

    feat(components)

---
+++ 03-props/src/components/cart.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const cart = () => {
+  return (
+    <>
+      <div className='parent'>
+      <div className='cart'>
+          <img src='https://images.unsplash.com/photo-1762088776943-28a9fbadcec4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzM3x8fGVufDB8fHx8fA%3D%3D'/>
+          <h1>Shivam Singh</h1>
+          <p>Software Developer</p>
+          <button>View Profile</button>
+      </div>
+      </div>
+     </>
+  )
+}
+
+export default cart
--- Import and Export/index.js
+++
@@ -1,4 +0,0 @@
-import sum from './sum.js'
-
-let solution = sum(10,20);
-console.log("The sum is "+ solution);
