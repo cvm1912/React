2da1b3e2278ade3c310bb0870a6008df05d1f7e5
cvm1912 <shivam.fcse@gmail.com>
2025-11-20 20:04:16 +0530

    concept(cascading style sheet): styling the css using component

---
+++ 05-css-styling/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/Button.css'
+
+const Button = () => {
+  return (
+    <div className='btn'>
+        Click
+    </div>
+  )
+}
+
+export default Button
---
+++ 05-css-styling/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Styles/Header.css'
+import '../Styles/Button.css'
+
+const Header = () => 
+{
+  return (
+    <div className='header'>
+       <h1>Shivam singh</h1>
+       <button className='btn'>login</button>
+    </div>
+  )
+}
+
+export default Header
